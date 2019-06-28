d4aee7278b0ed4792b051751537d0ad52ec45e3f
GermanAizek <GermanAizek@yandex.ru>
2019-06-28 14:09:48 +0300

    [FIX] Fixed scripts

--- bin/render.lua
+++ bin/render.lua
@@ -1,11 +1,10 @@
-﻿--SetBackground("content//textures//1.gif")
-SetBackground("content//textures//null.jpg")
-for i = 0, 50 do
-	CreatePerson("content//textures//1.gif", 0 + i^1.5, 0 + i*12)
-	CreatePerson("content//textures//1.gif", 1200 - i^1.5, 0 + i*12)
+﻿SetBackground("content//textures//null.jpg")
+
+for i = 0, 1400, 200 do
+	CreatePerson("content//textures//actor.png", i, 350)
+	--CreatePerson("content//textures//1.gif", 1200 - i^1.5, 0 + i*12)
 end
 --Write(path)
---SetColorSpace(0.9,0.5,0.7,1.0)
 --Write(1,2,3,4)
 --СreatePerson("content/textures/1.gif")
 --CreateBox("content/textures/1.jpg",100,100,1,1)
