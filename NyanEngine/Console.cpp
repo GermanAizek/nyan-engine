7fae03981ddaf8132c54b56431c646a85ccd4cbe
GermanAizek <GermanAizek@yandex.ru>
2019-08-03 03:51:18 +0300

    [ADD] Added PrintConsole and console commands
    [FIX] Fixed parameters in functions

--- NyanEngine/Console.cpp
+++ NyanEngine/Console.cpp
@@ -1,22 +1,44 @@
 #include "stdafx.h"
 #include "Console.h"
 
-void consoleCreate(const char* name)
+void consoleCreate(std::string_view name)
 {
-	ImGui::Begin(name);
-	ImGui::SetWindowPos(name, ImVec2(0, 0));
-	ImGui::SetWindowSize(name, ImVec2(WIDTH, (float)(HEIGHT * 0.18)));
-	ImGui::InputTextMultiline("", "Console", 255);
-	ImGui::InputText("", "", 255);
+	ImGui::Begin(name.data());
+	ImGui::SetWindowPos(name.data(), ImVec2(0, 0));
+	ImGui::SetWindowSize(name.data(), ImVec2(WIDTH, HEIGHT * 0.18));
+	ImGui::InputTextMultiline("", const_cast<char*>(consoleLine.c_str()), 4096, ImVec2(WIDTH, HEIGHT * 0.1), ImGuiInputTextFlags_ReadOnly);
+	ImGui::InputText("", const_cast<char*>(commandLine.c_str()), 1024, ImGuiInputTextFlags_AlwaysInsertMode);
 
-	if (ImGui::Button("Run")) {
-		//window.setTitle(windowTitle);
-	}
+	consoleHandler();
 
 	ImGui::End();
 }
 
+void printConsole(std::string_view text)
+{
+	consoleLine += text.data() + '\n';
+}
+
 size_t consoleHandler()
 {
-	
+	if (ImGui::Button("Run")) {
+		consoleLine += "> " + commandLine + '\n';
+
+		if (commandLine == "help")
+		{
+			consoleLine += "Nyan Engine\nDevelopers:\nHerman Semenov (GermanAizek)";
+		}
+		else if (commandLine == "clear")
+		{
+			consoleLine.clear();
+		}
+		else if (commandLine == "videomode")
+		{
+
+		}
+
+		commandLine.clear();
+	}
+
+	return 0;
 }
--- NyanEngine/Console.h
+++ NyanEngine/Console.h
@@ -2,7 +2,11 @@
 
 #include "Render.h"
 
-void consoleCreate(const char* name);
+void consoleCreate(std::string_view name);
 size_t consoleHandler();
+void printConsole(std::string_view text);
 
 bool consoleOpened = false;
+
+std::string consoleLine("");
+std::string commandLine("");
