07a0449bb3eac46720eecca95291cdccddb882dd
GermanAizek <GermanAizek@yandex.ru>
2019-09-25 14:59:14 +0300

    [FIX] Fixed Novel/Dialog.lua

--- bin/lua/GameAPI/Novel/dialog.lua
+++ bin/lua/GameAPI/Novel/dialog.lua
@@ -5,22 +5,25 @@
  * Written by Herman Semenov <GermanAizek@sensegames.ru>, September 2019
 ]]--
 
+local Render = require "render_functions"
+
 --- A module that provides a ready-made template for creating a simple dialogue.
 -- @module dialog
 local Dialog = {}
 
 --- Members dialog
--- @number dialog box width
--- @number dialog box height
+-- @number dialog box x
+-- @number dialog box y
 -- @number dialog texture width
 -- @number dialog texture height
 -- @number text size
 -- @string path to dialog body texture
 -- @string path to dialog header texture
-Dialog.width = ScrW() or nil
-Dialog.height = ScrH() * 0.25 or nil
+Dialog.x = ScrW() * 0.13 or nil
+Dialog.y = ScrH() * 0.68 or nil
 Dialog.sizeWidth = 1.0 or nil
 Dialog.sizeHeight = 1.0 or nil
+Dialog.nameFont = "content/fonts/arial.ttf" or nil
 Dialog.sizeFont = 38 or nil
 Dialog.bodyTexture = "content/textures/ui/dialogue_body.png" or nil
 Dialog.headerTexture = "content/textures/ui/dialogue_header.png" or nil
@@ -44,10 +47,10 @@ end
 --- show Render sprites body, header and texts.
 -- @return boolean flag
 function Dialog.show()
-	render.DrawSprite(Dialog.bodyTexture, Dialog.width, Dialog.height, Dialog.sizeWidth, Dialog.sizeHeight)
-	render.DrawSprite(Dialog.headerTexture, Dialog.width, Dialog.height, Dialog.sizeWidth, Dialog.sizeHeight)
-	draw.DrawText(text, Dialog.sizeFont, Dialog.width, Dialog.height)
-	draw.DrawText(text, Dialog.sizeFont, Dialog.width, Dialog.height)
+	Render.drawSprite(Dialog.bodyTexture, Dialog.x, Dialog.y, Dialog.sizeWidth, Dialog.sizeHeight)
+	Render.drawSprite(Dialog.headerTexture, Dialog.x, Dialog.y, Dialog.sizeWidth, Dialog.sizeHeight)
+	Render.drawText(text, Dialog.nameFont, Dialog.sizeFont, Dialog.x, Dialog.y)
+	Render.drawText(text, Dialog.nameFont, Dialog.sizeFont, Dialog.x, Dialog.y)
 end
 
 return Dialog
