ec69253a24d2d6976a48f712a3d468378c3e6b57
GermanAizek <GermanAizek@yandex.ru>
2019-10-31 00:42:45 +0300

    [ADD] Added test generated texture

--- NyanEngine/Texture.cpp
+++ NyanEngine/Texture.cpp
@@ -5,7 +5,27 @@ bool Nyan::Texture::loadFromFile(std::string_view file)
 {
 	if (!texture.loadFromFile(file.data()))
 	{
-		if (!texture.loadFromFile(ERROR_TEXTURE))
+		if (texture.create(2, 2))
+		{
+			// 2007
+			sf::Uint8* pixels = new sf::Uint8[2 * 2 * 4]; //RGBA	
+			/*pixels[0] = (255, 0, 220, 255);
+			pixels[1] = (0, 0, 0, 255);
+			pixels[2] = (255, 0, 220, 255);
+			pixels[3] = (0, 0, 0, 255);*/
+			for (size_t i = 0; i < 2 * 2 * 4; i += 2)
+			{
+				pixels[i] = 255;
+				pixels[i + 1] = 0;
+				pixels[i + 2] = 220;
+				pixels[i + 3] = 255;
+			}
+			texture.update(pixels);
+			//texture.setSmooth(true);
+			texture.setRepeated(true);
+			//delete[] pixels;
+		}
+		else
 			return false;
 	}
 
