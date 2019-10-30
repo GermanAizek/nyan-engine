c1cab028ba7f30e4b7defd5fa9b5bf5a7150cd9c
GermanAizek <GermanAizek@yandex.ru>
2019-10-30 20:02:21 +0300

    [FIX] Refactor and fix default value showEditor

--- NyanEngine/Engine.h
+++ NyanEngine/Engine.h
@@ -5,16 +5,16 @@ std::chrono::time_point<std::chrono::system_clock> systemClock;
 sf::Clock currentClock;
 
 namespace Core
-{ // В пространстве имен обязательно статические переменные
+{
 	extern bool criticalError = false;
 	extern bool benchmode = false;
 	extern bool debugmode = false;
-} // TODO: Косяк на косяке, но нельзя проверять режимы ядра движка в других файлах
+}
 
 namespace EngineEvent
 {
 	extern bool showStats = false;
-	extern bool showEditor = true;
+	extern bool showEditor = false;
 }
 
 int initEngine();
